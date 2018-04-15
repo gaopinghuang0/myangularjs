e5b8a692de53d40012ac39aff7082991c31c1313
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-15 17:54:39 -0400

    update README.md and add comments to src and test

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # myangularjs
-Implement my own angular js based on the book &lt;build your own angularjs>
+Implement my own angular js based on the book *Build your own angularjs* by Tero Parviainen. The source code of the book: <https://github.com/teropa/build-your-own-angularjs/>
 
 
-### Note
-Just use the package.json of the source code of the book on github.
+## Getting Started
+```bash
+$ npm install  # only for the first time 
+$ npm test  # start Karma
+# $ npm run lint  # run jshint separately, without using Karma
+```
 
-### Run
-npm test
+## Packages (from `Project Setup` part of the book)
+1. `jshint` - JS Lint
+2. `Jasmine` - Unit testing
+3. `Karma` - Test runner that integrates well with `jshint`, `Jasmine`, `Browserify` and `PhantomJS`
+4. `Sinon.JS` - Test helper for some of the more sophisticated mock objects e.g., HTTP features
+5. `Browserify` - Bring the module capabilities to client-side code (similar to CommonJS standard for Node.js). It will pre-process all our files and output a bundle that can be run in a web browser (such as the PhantomJS browser for testing).
+6. `Lo-Dash` - Array and object manipulation, such as equality checking and cloning.
+7. `jQuery` - DOM query and manipulation.
+
+
+## TODO
+Chapter 1 / Running Code After A Digest - $$postDigest
--- src/scope.js
+++ src/scope.js
@@ -3,155 +3,162 @@
 var _ = require('lodash');
 
 function Scope() {
-    this.$$watchers = [];
-    this.$$lastDirtyWatch = null;
-    this.$$asyncQueue = [];
-    this.$$applyAsyncQueue = [];
-    this.$$applyAsyncId = null;
-    this.$$postDigestQueue = [];
-    this.$$phase = null;
+  this.$$watchers = [];
+  this.$$lastDirtyWatch = null;  // optimization by short-circuit the digest
+  this.$$asyncQueue = [];
+  this.$$applyAsyncQueue = [];
+  this.$$applyAsyncId = null;
+  this.$$postDigestQueue = [];
+  this.$$phase = null;
 }
 
 // A function is not considered equal to anything but itself
 function initWatchVal() {}
 
+// Core function: attach a watcher to a scope
+// 
+// watchFn: a watch function which specifies the piece of data you’re interested in.
+//     Note: As a user, we will use a watch expression, not a watchFn, discussed in Chapter 5
+// listenerFn: a listener function which will be called whenever that data changes.
+// valueEq: a boolean flag to enable checking by value, not by reference
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
-    var watcher = {
-        watchFn: watchFn,
-        listenerFn: listenerFn || function() {},
-        valueEq: !!valueEq,
-        last: initWatchVal
-    };
-    this.$$watchers.push(watcher);
-    this.$$lastDirtyWatch = null;
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function() {},
+    valueEq: !!valueEq,
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+  // disable short-circuit optimization if a new watch is added during digest
+  this.$$lastDirtyWatch = null;  
 };
 
+// Core function: iterates over all the watchers that have been attached on the scope, 
+// and runs their watch and listener functions accordingly
 Scope.prototype.$digest = function() {
-    var ttl = 10;  // time to live, to prevent unstable chain watch
-    var dirty;
-    this.$$lastDirtyWatch = null;
-    this.$beginPhase('$digest');
+  var ttl = 10;  // time to live, to prevent unstable chain watch
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  this.$beginPhase('$digest');
 
-    // flush applyAsync if call $digest first
-    if (this.$$applyAsyncId) {
-        clearTimeout(this.$$applyAsyncId);
-        this.$$flushApplyAsync();
+  // flush applyAsync if call $digest first
+  if (this.$$applyAsyncId) {
+    clearTimeout(this.$$applyAsyncId);
+    this.$$flushApplyAsync();
+  }
+
+  do {
+    // this guarantees that the function will be invoked later 
+    // but still during the same digest
+    while (this.$$asyncQueue.length) {
+      var asyncTask = this.$$asyncQueue.shift();
+      asyncTask.scope.$eval(asyncTask.expression);
     }
-
-    do {
-        // this guarantees that the function will be invoked later 
-        // but still during the same digest
-        while (this.$$asyncQueue.length) {
-            var asyncTask = this.$$asyncQueue.shift();
-            asyncTask.scope.$eval(asyncTask.expression);
-        }
-        dirty = this.$$digestOnce();
-        if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
-            throw "10 digest iterations reached";
-        }
-    } while (dirty || this.$$asyncQueue.length);
-    this.$clearPhase();
-
-    while (this.$$postDigestQueue.length) {
-        this.$$postDigestQueue.shift()();
+    dirty = this.$$digestOnce();
+    if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+      throw "10 digest iterations reached";
     }
+  } while (dirty || this.$$asyncQueue.length);
+  this.$clearPhase();
+
+  while (this.$$postDigestQueue.length) {
+    this.$$postDigestQueue.shift()();
+  }
 };
 
-
+// iterate all watchers once and return whether there were any changes or not
 Scope.prototype.$$digestOnce = function() {
-    var self = this;
-    var newValue, oldValue, dirty;
-    _.forEach(this.$$watchers, function(watcher) {
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-            self.$$lastDirtyWatch = watcher;
-            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-            watcher.listenerFn(newValue, 
-                // For new watches, we should provide the new value as the old value
-                (oldValue === initWatchVal ? newValue : oldValue),
-                self);
-            dirty = true;
-        } else if (self.$$lastDirtyWatch === watcher) {
-            return false;  // optimization, stop early
-        }
-    });
-    return dirty;
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+      self.$$lastDirtyWatch = watcher;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+      watcher.listenerFn(newValue, 
+        // For new watches, we should provide the new value as the old value
+        (oldValue === initWatchVal ? newValue : oldValue),
+        self);
+      dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;  // optimization, stop early
+    }
+  });
+  return dirty;
 };
 
 Scope.prototype.$beginPhase = function(phase) {
-    if (this.$$phase) {
-        throw this.$$phase + ' already in progress.';
-    }
-    this.$$phase = phase;
+  if (this.$$phase) {
+    throw this.$$phase + ' already in progress.';
+  }
+  this.$$phase = phase;
 };
 
 Scope.prototype.$clearPhase = function() {
-    this.$$phase = null;
+  this.$$phase = null;
 };
 
 Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
-    if (valueEq) {
-        return _.isEqual(newValue, oldValue);
-    } else {
-        return newValue === oldValue ||
-            (typeof newValue === 'number' && typeof oldValue === 'number' &&
-                isNaN(newValue) && isNaN(oldValue));
-    }
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue ||
+      (typeof newValue === 'number' && typeof oldValue === 'number' &&
+        isNaN(newValue) && isNaN(oldValue));
+  }
 };
 
 Scope.prototype.$eval = function(expr, locals) {
-    return expr(this, locals);
+  return expr(this, locals);
 };
 
 Scope.prototype.$apply = function(expr) {
-    try {
-        this.$beginPhase('$apply');
-        return this.$eval(expr);
-    } finally {
-        this.$clearPhase();
-        this.$digest();
-    }
+  try {
+    this.$beginPhase('$apply');
+    return this.$eval(expr);
+  } finally {
+    this.$clearPhase();
+    this.$digest();
+  }
 };
 
-
-// $evalAsync, on the other
-// hand, is much more strict about when the scheduled work is executed. Since it’ll happen
-// during the ongoing digest, it’s guaranteed to run before the browser decides to do anything
-// else.
+// $evalAsync, on the other hand, is much more strict about when the scheduled work is executed.
+// Since it’ll happen during the ongoing digest, it’s guaranteed to run 
+// before the browser decides to do anything else.
 Scope.prototype.$evalAsync = function(expr) {
-    var self = this;
-    if (!self.$$phase && !self.$$asyncQueue.length) {
-        setTimeout(function() {
-            if (self.$$asyncQueue.length) {
-                self.$digest();
-            }
-        }, 0);
-    }
-    this.$$asyncQueue.push({scope: this, expression: expr});
+  var self = this;
+  if (!self.$$phase && !self.$$asyncQueue.length) {
+    setTimeout(function() {
+      if (self.$$asyncQueue.length) {
+        self.$digest();
+      }
+    }, 0);
+  }
+  this.$$asyncQueue.push({scope: this, expression: expr});
 };
 
 Scope.prototype.$applyAsync = function(expr) {
-    var self = this;
-    self.$$applyAsyncQueue.push(function() {
-        self.$eval(expr);
-    });
-    if (self.$$applyAsyncId === null) {
-        self.$$applyAsyncId = setTimeout(function() {
-            self.$apply(_.bind(self.$$flushApplyAsync, self));
-        }, 0);
-    }
+  var self = this;
+  self.$$applyAsyncQueue.push(function() {
+    self.$eval(expr);
+  });
+  if (self.$$applyAsyncId === null) {
+    self.$$applyAsyncId = setTimeout(function() {
+      self.$apply(_.bind(self.$$flushApplyAsync, self));
+    }, 0);
+  }
 };
 
 Scope.prototype.$$flushApplyAsync = function() {
-    while (this.$$applyAsyncQueue.length) {
-        this.$$applyAsyncQueue.shift()();
-    }
-    this.$$applyAsyncId = null;
+  while (this.$$applyAsyncQueue.length) {
+    this.$$applyAsyncQueue.shift()();
+  }
+  this.$$applyAsyncId = null;
 };
 
 Scope.prototype.$$postDigest = function(fn) {
-    this.$$postDigestQueue.push(fn);
+  this.$$postDigestQueue.push(fn);
 };
 
 module.exports = Scope;
