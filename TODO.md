a6f81faf07fd46bbd4d495e454adf1da7f0b4c76
gaopinghuang0 <gaopinghuang@gmail.com>
2018-05-03 13:12:09 -0400

    chapter 3 is done

--- TODO.md
+++ TODO.md
@@ -1,2 +1,7 @@
 ## TODO
-Chapter 3 Watching Collections
+Chapter 4 Scope Events
+
+## DONE
+Chapter 1  - Scopes and Digest
+Chapter 2  - Scope Inheritance
+Chapter 3  - Watching Collections
--- src/scope.js
+++ src/scope.js
@@ -300,4 +300,108 @@ Scope.prototype.$destroy = function() {
   this.$$watchers = null;
 };
 
+function isArrayLike(obj) {
+  if (_.isNull(obj) || _.isUndefined(obj)) {
+    return false;
+  }
+  var length = obj.length;
+  // consider the obj has a property called length, say obj.length is 42
+  // an array will have 41, while a non-array obj is not
+  // this check works for most objects, well enough for practice
+  return length === 0 || (_.isNumber(length) && length > 0 && (length-1) in obj);
+}
+
+// chapter 3 watch collections
+Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
+  var self = this;
+  var newValue, oldValue;
+  var oldLength;  // optimization
+  var changeCount = 0;  // guarantees that internalWatchFn will return a diff value if changes
+  var veryOldValue;
+  // only track veryOldValue if listenerFn needs to use old value
+  // because track is expensive
+  var trackVeryOldValue = (listenerFn.length > 1);
+  var firstRun = true;
+
+  var internalWatchFn = function(scope) {
+    var newLength;
+    newValue = watchFn(scope);
+
+    // check for changes
+    if (_.isObject(newValue)) {
+      if (isArrayLike(newValue)) {
+        if (!_.isArray(oldValue)) {
+          changeCount++;
+          oldValue = [];
+        }
+        if (newValue.length !== oldValue.length) {
+          changeCount++;
+          oldValue.length = newValue.length;
+        }
+        _.forEach(newValue, function(newItem, i) {
+          var bothNaN = _.isNaN(newItem) && _.isNaN(oldValue[i]);
+          if (!bothNaN && newItem !== oldValue[i]) {
+            changeCount++;
+            oldValue[i] = newItem;
+          }
+        });
+      } else {
+        if (!_.isObject(oldValue) || isArrayLike(oldValue)) {
+          changeCount++;
+          oldValue = {};
+          oldLength = 0;
+        }
+        newLength = 0;
+        _.forOwn(newValue, function(newVal, key) {
+          newLength++;
+          if (oldValue.hasOwnProperty(key)) {
+            var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+            if (!bothNaN && oldValue[key] !== newVal) {
+              changeCount++;
+              oldValue[key] = newVal;
+            }
+          } else {
+            changeCount++;
+            oldLength++;
+            oldValue[key] = newVal;
+          }
+        });
+        if (oldLength > newLength) {
+          changeCount++;
+          _.forOwn(oldValue, function(oldVal, key) {
+            if (!newValue.hasOwnProperty(key)) {
+              oldLength--;
+              delete oldValue[key];
+            }
+          });
+        }
+      }
+    } else {
+      if (!self.$$areEqual(newValue, oldValue, false)) {
+        changeCount++;
+      }
+      oldValue = newValue;    
+    }
+
+
+
+    return changeCount;
+  };
+  var internalLisenterFn = function() {
+    if (firstRun) {
+      listenerFn(newValue, newValue, self);
+      firstRun = false;
+    } else {
+      listenerFn(newValue, veryOldValue, self);
+    }
+
+    if (trackVeryOldValue) {
+      veryOldValue = _.clone(newValue);
+    }
+  };
+  return this.$watch(internalWatchFn, internalLisenterFn);
+};
+
+
+
 module.exports = Scope;
