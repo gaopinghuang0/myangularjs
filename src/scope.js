1eb420c011ea379743b5858d7be8569b7e621867
gaopinghuang0 <gaopinghuang@gmail.com>
2018-05-04 15:11:58 -0400

    chapter 4 is done

--- src/scope.js
+++ src/scope.js
@@ -11,6 +11,7 @@ function Scope() {
   this.$$postDigestQueue = [];
   this.$root = this;
   this.$$children = [];
+  this.$$listeners = {};
   this.$$phase = null;  // $evalAsync uses it to know whether it's in a $digest phase; if not, schedule one
 }
 
@@ -284,12 +285,14 @@ Scope.prototype.$new = function(isolated, parent) {
   }
   parent.$$children.push(child);
   child.$$watchers = [];
+  child.$$listeners = {};
   child.$$children = [];
   child.$parent = parent;
   return child;
 };
 
 Scope.prototype.$destroy = function() {
+  this.$broadcast('$destroy');
   if (this.$parent) {
     var siblings = this.$parent.$$children;
     var indexOfThis = siblings.indexOf(this);
@@ -298,6 +301,7 @@ Scope.prototype.$destroy = function() {
     }
   }
   this.$$watchers = null;
+  this.$$listeners = {};
 };
 
 function isArrayLike(obj) {
@@ -383,8 +387,6 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
       oldValue = newValue;    
     }
 
-
-
     return changeCount;
   };
   var internalLisenterFn = function() {
@@ -402,6 +404,79 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
   return this.$watch(internalWatchFn, internalLisenterFn);
 };
 
+// chapter 4 Events
+Scope.prototype.$on = function(eventName, listener) {
+  var listeners = this.$$listeners[eventName];
+  if (!listeners) {
+    this.$$listeners[eventName] = listeners = [];
+  }
+  listeners.push(listener);
+  return function() {
+    var index = listeners.indexOf(listener);
+    if (index >= 0) {
+      listeners[index] = null;
+    }
+  };
+};
 
+Scope.prototype.$emit = function(eventName) {
+  var propagationStopped = false;
+  var event = {
+    name: eventName,
+    targetScope: this,
+    stopPropagation: function() {
+      propagationStopped = true;
+    },
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  // var additionalArgs = _.rest(arguments);  // _.rest API is changed to _.tail
+  var listenerArgs = [event].concat(_.tail(arguments));
+  var scope = this;
+  do {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    scope = scope.$parent;
+  } while (scope && !propagationStopped);
+  event.currentScope = null;
+  return event;
+};
+
+Scope.prototype.$broadcast = function(eventName) {
+  var event = {
+    name: eventName,
+    targetScope: this,
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  // var additionalArgs = _.rest(arguments);  // _.rest API is changed to _.tail
+  var listenerArgs = [event].concat(_.tail(arguments));
+  this.$$everyScope(function(scope) {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    return true;
+  });
+  event.currentScope = null;
+  return event;
+};
+
+Scope.prototype.$$fireEventOnScope = function(eventName, listenerArgs) {
+  var listeners = this.$$listeners[eventName] || [];
+  var i = 0;
+  while (i < listeners.length) {
+    if (listeners[i] === null) {
+      listeners.splice(i, 1);
+    } else {
+      try {
+        listeners[i].apply(null, listenerArgs);
+      } catch (e) {
+        console.error(e);
+      }
+      i++;
+    }
+  }
+};
 
 module.exports = Scope;
